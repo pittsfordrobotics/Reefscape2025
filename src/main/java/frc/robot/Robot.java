a1ca58aac56012feb74bcc2ab4bfdefdb3e410b2
James Mou <95831855+Hammyboi@users.noreply.github.com>
2024-07-09 20:05:41 -0400

    started subsystems and added yagsl

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,9 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import org.littletonrobotics.junction.LoggedRobot;
+
+import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -14,7 +16,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
@@ -25,6 +27,15 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    // Make sure you only configure port forwarding once in your robot code.
+    // Do not place these function calls in any periodic functions
+    for (int port = 5800; port <= 5807; port++) {
+      PortForwarder.add(port, "limelight.local", port);
+    }
+
+    // UsbCamera camera = CameraServer.startAutomaticCapture();
+    // camera.setVideoMode(PixelFormat.kYUYV, 640, 480, 10); // TODO: Camera is disabled for FLR, change if needed later
+    
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
