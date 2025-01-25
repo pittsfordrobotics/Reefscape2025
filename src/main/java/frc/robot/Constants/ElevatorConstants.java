ce8c426336880bc1dadc575306addb6ef75fadc3
libby du <libbyduhappy@gmail.com>
2025-01-24 20:20:29 -0500

    added temp elevator constants

--- src/main/java/frc/robot/Constants/ElevatorConstants.java
+++ src/main/java/frc/robot/Constants/ElevatorConstants.java
@@ -2,5 +2,7 @@ package frc.robot.Constants;
 
 public class ElevatorConstants {
     public static final int CAN_ELEVATOR_MOTOR = 31;
+    public static final double ELEVATOR_MAX_HEIGHT = 100; // need to change + decide units
+    public static final double SHUTTLE_LENGTH = 20; // ^^^^^^
 
 }
