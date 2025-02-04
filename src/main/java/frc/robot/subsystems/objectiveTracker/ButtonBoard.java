2c57af1e616470953055f68904c162d6d642d226
James Mou <95831855+Hammyboi@users.noreply.github.com>
2025-02-03 20:42:07 -0500

    Fixed negative modulus bug and added comments

--- src/main/java/frc/robot/subsystems/objectiveTracker/ButtonBoard.java
+++ src/main/java/frc/robot/subsystems/objectiveTracker/ButtonBoard.java
@@ -14,29 +14,32 @@ public class ButtonBoard extends SubsystemBase {
   private int level = 1;
   private boolean rightSide = false;
   private int reefSide = 0;
-  /** Creates a new ButtonBoard. */
+  /** This class is used to control the Objective Selector with the button board. */
   public ButtonBoard(BooleanSupplier[] buttonSupplier) {
     this.buttonSupplier = buttonSupplier;
   }
 
   @Override
   public void periodic() {
+    // Take button inputs and update the level, rightSide, and reefSide variables
     for(int i = 0; i < buttonSupplier.length; i++) {
       buttons[i] = buttonSupplier[i].getAsBoolean();
     }
 
+    // Update level
     for(int i=5; i<=8; i++) {
       if(buttons[i]) {
         level = 9 - i;
       }
     }
 
+    // Check if the left/right buttons are pressed and decide which side the robot is on
     if(buttons[1] || buttons[2]) rightSide = buttons[1];
     
     if(buttons[3]) {
       reefSide++;
     } else if(buttons[4]) {
-      reefSide--;
+      reefSide+=5; // have to do this so that reefSide never goes negative, basically subtract 1
     }
     reefSide %= 6; //maintain 0-6 range
   }
@@ -48,7 +51,7 @@ public class ButtonBoard extends SubsystemBase {
   public boolean getRightSide() {
     return rightSide;
   }
-
+  /** @return The side of the reef the robot is on, to be shown in the ObjectiveTracker app */
   public int getReefSide() {
     return reefSide;
   }
