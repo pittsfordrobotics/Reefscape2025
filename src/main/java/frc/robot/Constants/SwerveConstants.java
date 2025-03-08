a8cf6b1812ecca68322d6cbd16360ba7b5eeec15
James Mou <95831855+Hammyboi@users.noreply.github.com>
2025-03-08 17:08:23 -0500

    Updated swerve drive motor constants and set angle friction to 0.55

--- src/main/java/frc/robot/Constants/SwerveConstants.java
+++ src/main/java/frc/robot/Constants/SwerveConstants.java
@@ -3,10 +3,10 @@ package frc.robot.Constants;
 import frc.robot.lib.SwerveModuleConstants;
 
 public class SwerveConstants {
-    private static SwerveModuleConstants FRONT_LEFT_CONSTANTS = new SwerveModuleConstants(0.3199, 2.6512, 0.50856, 0, 0, 0);
-    private static SwerveModuleConstants FRONT_RIGHT_CONSTANTS = new SwerveModuleConstants(0.23351, 2.6361, 0.69398, 0, 0, 0);
-    private static SwerveModuleConstants BACK_LEFT_CONSTANTS = new SwerveModuleConstants(0.26965, 2.6011, 0.67263, 0, 0, 0);
-    private static SwerveModuleConstants BACK_RIGHT_CONSTANTS = new SwerveModuleConstants(0.25825, 2.7213, 0.63848, 0, 0, 0);
+    private static SwerveModuleConstants FRONT_LEFT_CONSTANTS = new SwerveModuleConstants(0.157527, 2.0141, 0.14898, 0, 0, 0);
+    private static SwerveModuleConstants FRONT_RIGHT_CONSTANTS = new SwerveModuleConstants(0.14548, 1.9997, 0.18201, 0, 0, 0);
+    private static SwerveModuleConstants BACK_LEFT_CONSTANTS = new SwerveModuleConstants(0.15314, 1.9436, 0.1457, 0, 0, 0);
+    private static SwerveModuleConstants BACK_RIGHT_CONSTANTS = new SwerveModuleConstants(0.14703, 2.0077, 0.18546, 0, 0, 0);
     public static final SwerveModuleConstants[] MODULE_CONSTANTS = {FRONT_LEFT_CONSTANTS, FRONT_RIGHT_CONSTANTS, BACK_LEFT_CONSTANTS, BACK_RIGHT_CONSTANTS};
 
     public static double SWERVE_MAXIMUM_VELOCITY = 5.5;
