935f18c39ab392cef5bac8e45a1da94b314cc7e4
James Mou <95831855+Hammyboi@users.noreply.github.com>
2025-03-25 18:40:31 -0400

    Add number for ground pickup

--- src/main/java/frc/robot/Constants/AlgaeConstants.java
+++ src/main/java/frc/robot/Constants/AlgaeConstants.java
@@ -20,6 +20,7 @@ public class AlgaeConstants {
 
     public static final double PIVOT_STORE_DEGREES = 0; // Set up zero on encoder with algae arm up
     public static final double PIVOT_DOWN_DEGREES = -20;
+    public static final double PIVOT_GROUND_DEGREES = -40;
 
     public static final double PICKUP_INTAKE_SPEED = -0.5;
     public static final double PICKUP_OUTTAKE_SPEED = 0.5;
